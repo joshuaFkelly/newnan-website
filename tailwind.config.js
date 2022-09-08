b8f38464a704a99e44e70ff149109270218f5d38
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-09-08 13:53:47 -0400

    trying to fix production errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
       },
     },
   },
+  corePlugins: {
+    aspectRatio: false,
+  },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
